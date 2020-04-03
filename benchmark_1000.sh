b51d7e4dd8e9aa02598dda0a35b2027693d09f89
taelee <taelee@student.42seoul.kr>
2020-04-03 16:16:37 +0900

    benchmark added

---
+++ benchmark_1000.sh
@@ -0,0 +1,14 @@
+cd ..
+start_time=`date +%s.$N`
+start_time_string=`date`
+
+	for (( i = 4294966296; i <= 4294967295; i++ )) do
+		./factorization $i >> temp_benchmark
+	done
+end_time=`date +%s.$N`
+end_time_string=`date`
+elapsed_time=`echo "$end_time - $start_time" | bc`
+htime=`echo "$elapsed_time/3600" | bc`
+mtime=`echo "($elapsed_time/60) - ($htime * 60)" | bc`
+stime=`echo "$elapsed_time - (($elapsed_time/60) * 60)" | bc`
+echo "Total time : ${htime} H ${mtime} M ${stime} S"
